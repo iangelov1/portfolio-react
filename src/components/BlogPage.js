aeeb024263511a49666a59203c74212b999f281c
ivan-angelov <62991796+ivan-angelov@users.noreply.github.com>
2022-02-10 16:03:35 +0200

    commit 2:33:03

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,10 +1,67 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+import img from '../assets/Images/patrick-tomasso-Oaqk7qqNh_c-unsplash.jpg'; 
+
+// components
+import LogoComponent from '../subComponents/LogoComponent';
+import PowerButton from '../subComponents/PowerButton';
+import SocialIcons from '../subComponents/SocialIcons';
+import BlogComponent from './BlogComponent';
+
+import { Blogs } from '../data/BlogData';
+
+const MainContainer = styled.div`
+    background-image: url(${img});
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-position: center;
+    // width: 100vw;
+`;
+
+const Container = styled.div`
+    background-color: ${props => `rgba(${props.theme.bodyRgba}, 0.8)`};
+    width: 100%;
+    height: auto;
+    position: relative;
+    padding-bottom: 5rem;
+
+`;
+
+const Center = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-top: 10rem;
+`;
+
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, minmax(calc(10rem + 15vw), 1fr));
+    grid-grap: calc(1rem + 2vw);
+
+`;
 
 const BlogPage = () => {
     return (
-        <div>
-            BlogPage
-        </div>
+        <MainContainer>
+            <Container>
+                <LogoComponent />
+                <PowerButton  />
+                <SocialIcons />
+
+                <Center>
+                    <Grid>
+                        {
+                            Blogs.map((blog) => {
+                                return <BlogComponent key={blog.id} blog={blog} />
+                            })
+                        }
+                    </Grid>
+                </Center>
+            </Container>
+        </MainContainer>
     )
 }
 
--- src/subComponents/SocialIcons.js
+++ src/subComponents/SocialIcons.js
@@ -3,6 +3,8 @@ import { NavLink } from 'react-router-dom'
 import styled from 'styled-components';
 import { Facebook, Github, Twitter, YouTube } from '../components/AllSvgs';
 
+import { DarkTheme } from '../components/Themes';
+
 const Icons = styled.div`
     display: flex;
     flex-direction: column;
@@ -21,37 +23,37 @@ const Icons = styled.div`
 const Line = styled.span`
     width: 2px;
     height: 8rem;
-    background-color: ${props => props.theme.text};
+    background-color: ${props => props.color === "dark" ? DarkTheme.text : DarkTheme.body};
 `;
 
-const SocialIcons = () => {
+const SocialIcons = (props) => {
     return (
         <Icons>
             <div>
                 <NavLink to="/" style={{ color: 'inherit' }}>
-                    <Github width={25} height={25} fill='currentColor' />
+                    <Github width={25} height={25} fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body } />
                 </NavLink>
             </div>
 
             <div>
                 <NavLink to="/" style={{ color: 'inherit' }}>
-                    <Twitter width={25} height={25} fill='currentColor' />
+                    <Twitter width={25} height={25} fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body } />
                 </NavLink>
             </div>
 
             <div>
                 <NavLink to="/" style={{ color: 'inherit' }}>
-                    <Facebook width={25} height={25} fill='currentColor' />
+                    <Facebook width={25} height={25} fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body } />
                 </NavLink>
             </div>
 
             <div>
                 <NavLink to="/" style={{ color: 'inherit' }}>
-                    <YouTube width={25} height={25} fill='currentColor' />
+                    <YouTube width={25} height={25} fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body } />
                 </NavLink>
             </div>
 
-            <Line />
+            <Line color={props.theme} />
         </Icons>
     )
 }
